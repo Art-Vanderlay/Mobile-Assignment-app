96360a0e910cdc85a4583bea26e139276aca1f56
Art-Vanderlay <tomsarantis@gmail.com>
2017-10-30 22:30:09 +1100

    Add files via upload
    Contents of IoTLeakDetector\app\src\main

---
+++ java/au/edu/vu/iotleakdetector/WebViewActivity.java
@@ -0,0 +1,40 @@
+package au.edu.vu.iotleakdetector;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+        myWebView.loadUrl("https://www.vu.edu.au/");
+
+        myWebView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    public class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /** Show a toast from the web page */
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
